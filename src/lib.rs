96b406f3ec5acf548ab2820be4e0f3286f12886f
Bart Massey <bart@cs.pdx.edu>
2019-08-23 14:56:13 -0700

    fixed SeqCst visibility

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ static DEBUG: AtomicBool = AtomicBool::new(false);
 #[macro_export]
 macro_rules! trace {
     ($fmt:expr, $($args:expr),*) => {
+        use std::sync::atomic::Ordering::SeqCst;
         if DEBUG.load(SeqCst) {
             println!($fmt, $($args),*);
         }
