06db20c1abb67c984b4a48eeec7067dec551e5bb
Bart Massey <bart@cs.pdx.edu>
2019-08-23 14:58:59 -0700

    fixed DEBUG visibility, scoping

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 use std::sync::atomic::{AtomicBool, Ordering::SeqCst};
 
-static DEBUG: AtomicBool = AtomicBool::new(false);
+pub static DEBUG: AtomicBool = AtomicBool::new(false);
 
 #[macro_export]
 macro_rules! trace {
-    ($fmt:expr, $($args:expr),*) => {
+    ($fmt:expr, $($args:expr),*) => {{
+        use crate::DEBUG;
         use std::sync::atomic::Ordering::SeqCst;
         if DEBUG.load(SeqCst) {
             println!($fmt, $($args),*);
         }
-    }
+    }}
 }
 
 pub fn trace(status: bool) {
